1256fa71be9f0e5ebb12544d27cfff077db73bf1
Annu-Ag <annuag96@gmail.com>
2025-08-22 12:27:31 +0530

    Done modification with event

--- 16ColorPickerJS/script.js
+++ 16ColorPickerJS/script.js
@@ -4,8 +4,27 @@ const cyan = document.querySelector(".cyan");
 const violet = document.querySelector(".violet");
 const orange = document.querySelector(".orange");
 
+const center = document.querySelector(".center");
+
 const bgColor = (selcetedElement) =>{
     return window.getComputedStyle(selcetedElement).backgroundColor;
 }
 
-console.log(bgColor(pink));
+// let pinkElementColor = bgColor(pink);
+
+// pink.addEventListener("mouseenter",() =>{
+//     center.style.background = pinkElementColor;
+// });
+
+const magicColorChanger = (element , color) =>{
+    return element.addEventListener("mouseenter",() =>{
+    center.style.background = color;
+});
+
+}
+
+magicColorChanger(red, bgColor(red) );
+magicColorChanger(orange, bgColor(orange) );
+magicColorChanger(pink, bgColor(pink) );
+magicColorChanger(cyan, bgColor(cyan) );
+magicColorChanger(violet, bgColor(violet) );
